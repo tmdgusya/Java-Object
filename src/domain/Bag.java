eae49cb0b0fdd72a810aadd6172fa5b726faa632
roach <tmdgusya@naver.com>
2021-10-23 17:43:35 +0900

    feat: Bag Class 추가

---
+++ src/domain/Bag.java
@@ -0,0 +1,39 @@
+package domain;
+
+/**
+ * 관람객이 소지품을 보관하고 있는 Bag
+ */
+public class Bag {
+    private Long amount;
+    private Invitation invitation;
+    private Ticket ticket;
+
+    public Bag(long amount) {
+        this(null, amount);
+    }
+
+    public Bag(Invitation invitation, long amount) {
+        this.invitation = invitation;
+        this.amount = amount;
+    }
+
+    public boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    public boolean hasTicket() {
+        return ticket != null;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+}
