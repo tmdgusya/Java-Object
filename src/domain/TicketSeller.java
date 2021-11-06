732f2dfc0e19211385b9f11ac66d58a97cbd63e3
roach <tmdgusya@naver.com>
2021-11-06 18:42:27 +0900

    feat: TicketSeller 구현

---
+++ src/domain/TicketSeller.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class TicketSeller {
+
+    private final TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+
+}
