fb43889ee3443ebbca1f2f4ae49375e8e1d7e8df
roach <tmdgusya@naver.com>
2021-11-21 16:43:07 +0900

    discountPolicy 없을때 null 처리 추가

--- src/chapter02/domain/Movie.java
+++ src/chapter02/domain/Movie.java
@@ -21,6 +21,9 @@ public class Movie {
     }
 
     public Money calculateMovieFee(Screening screening) {
+        if (discountPolicy == null) {
+            return fee;
+        }
         return fee.minus(discountPolicy.calculateDiscountAmount(screening));
     }
 
